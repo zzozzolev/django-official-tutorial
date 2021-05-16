340b3d4cf940f20c529cf26dc0f64744234603bf
hyemi-noh <hyemi.noh@navercorp.com>
2021-05-16 11:41:10 +0900

    Add api-auth path to urls.py

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
@@ -10,3 +10,7 @@ urlpatterns = [
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
+
+urlpatterns += [
+    path('api-auth/', include('rest_framework.urls')),
+]
