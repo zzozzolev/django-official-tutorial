2da883e4de8ffc4bda4ff99aa13821306827eded
zzlev <gpalektzja@gmail.com>
2021-05-19 18:37:25 +0900

    Add SnippetHighlight to views

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('', views.api_root),
     path('snippets/', views.SnippetList.as_view(), name='snippet-list'),
     path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),
+    path('snippets/<int:pk>/highlight/',
+         views.SnippetHighlight.as_view(), name='snippet-highlight'),
     path('users/', views.UserDetail.as_view(), name='user-list'),
     path('users/<int:pk>', views.UserDetail.as_view())
 ]
--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import User
-from rest_framework import generics
-from rest_framework import permissions
+from rest_framework import generics, permissions, renderers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
@@ -32,6 +31,15 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = SnippetSerializer
     permission_classes = [
         permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
 
 class UserList(generics.ListAPIView):
